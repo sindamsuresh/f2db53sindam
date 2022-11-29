9f59e04e49c0bc025d68bac718100420c8991b38
sureshsindam <40721107+sindamsuresh@users.noreply.github.com>
2022-11-29 01:42:53 -0600

    made changes in login and added authentication

--- routes/mercedesBenz.js
+++ routes/mercedesBenz.js
@@ -21,8 +21,21 @@ router.get('/detail', mercedesBenz_controller.mercedesBenz_view_one_Page);
 /* GET create mercedesBenz page */
 router.get('/create', mercedesBenz_controller.mercedesBenz_create_Page);
 
+
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+    return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    }
+    
 /* GET create update page */
-router.get('/update', mercedesBenz_controller.mercedesBenz_update_Page);
+router.get('/update', secured, mercedesBenz_controller.mercedesBenz_update_Page);
+
+
 
 /* GET delete mercedesBenz page */
 router.get('/delete', mercedesBenz_controller.mercedesBenz_delete_Page);
